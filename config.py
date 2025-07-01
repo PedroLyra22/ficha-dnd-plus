061397b326f3ac543e1087bfd28dc0de7c7c6324
Pedro <pedrocalezanelyra@gmail.com>
2025-07-01 18:31:38 -0300

    feat: setup do padrão de projetos Factory Aplication Method

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = '12981709210dcc27bbbf43666280178c'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app', 'dndplus.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
